fc64948639bc3ace7032e12fc6e6e720ee5e2c7f
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-15 09:28:43 +0200

    seperated router into respective files

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
 
 require "functions.php";
-
-$url_array = parse_url($_SERVER["REQUEST_URI"]);
-$url = $url_array["path"];
-//dd(parse_url($_SERVER["REQUEST_URI"]));
-
-if ($url == "/") {
-    require "controllers/index.php";
-}
-elseif ($url == "/collectives") {
-    require "controllers/collectives.php";
-} else {
-    http_response_code(404);
-    require "controllers/404.php";
-}
+require "routes.php";
+require "router.php";
 
 ?>
---
+++ routes.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$routes = [
+    "/" => "controllers/index.php",
+    "/collectives" => "controllers/collectives.php"
+];
+
+
+
+?>
